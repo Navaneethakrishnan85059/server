10e65cd2717119739c1af4b794ffceaf83207b2a
Navaneethakrishnan85059 <navaneethakrishnan85059@gmail.com>
2024-11-14 21:26:05 +0530

    reload server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-const port: number = 6000;
+const port: number = 8080;
 
 
 
+app.use(cors());
+app.use(express.json());
 
-app.listen(() => {
+
+app.listen(port,() => {
   console.log("port :>> ", port);
 });
