5395adbe2ecc165d3b127c3485777587c4a31743
Navaneethakrishnan85059 <navaneethakrishnan85059@gmail.com>
2024-11-15 22:52:37 +0530

    initial commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,2 @@
 
-import { NextFunction, Request, Response } from "express";
 
-function errorHandler(err:Error,req:Request,res:Response,next:NextFunction){
-
-    if(res.headersSent)
-{
-    return next(err)
-}
-
-res.status(500)
-res.render('error',{errors:err})
-}
-
-
-export default errorHandler
